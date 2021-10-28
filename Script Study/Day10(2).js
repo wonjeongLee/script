6c26138593b2e3c4b0400e4f23d29064df1e4ac4
WonJeongLee <lwj010101517@gmail.com>
2021-10-28 22:49:04 +0900

    Script 코딩테스트 문제 풀이 20211028

---
+++ Script Study/Day10(2).js
@@ -0,0 +1,13 @@
+//등수 구하기
+function solution(arr){
+    let max = 0;
+    let c = 0;
+    for(let i = 1 ; i < arr.length; i++){
+        max = arr[0];
+        if(max < arr[i]){
+            max = arr[i]
+        }
+    }
+}
+let arr=[87, 89 ,92 ,100 ,76];
+console.log(solution(arr));
