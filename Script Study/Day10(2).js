cb0bbeacdecbe7467e63b1224d2b785285f7f9d6
Wonjeong lee <lwj010101517@gmail.com>
2021-10-29 23:46:25 +0900

    script 파일 설정

--- Script Study/Day10(2).js
+++ Script Study/Day10(2).js
@@ -2,12 +2,14 @@
 function solution(arr){
     let max = 0;
     let c = 0;
-    for(let i = 1 ; i < arr.length; i++){
-        max = arr[0];
-        if(max < arr[i]){
-            max = arr[i]
-        }
-    }
+    // for(let i = 1 ; i < arr.length; i++){
+    //     max = arr[0];
+    //     if(max < arr[i]){
+    //         max = arr[i];
+    //     }
+    // }
+    return max;
 }
 let arr=[87, 89 ,92 ,100 ,76];
 console.log(solution(arr));
+
