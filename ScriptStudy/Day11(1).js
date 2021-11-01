8b24bda548d4639207d1bbb1b8fdb8919882e53b
WonJeongLee <lwj010101517@gmail.com>
2021-11-01 22:59:45 +0900

    script 파일 추가 및 문제풀이

--- ScriptStudy/Day11(1).js
+++ ScriptStudy/Day11(1).js
@@ -3,37 +3,29 @@ function solution(arr){
     let first = 0;
     let second = 0;
     let third = 0;
-
-    let max = [];
+    let third1 =0;
 
     for(let i = 0; i < arr.length ; i++){
         for(let j = 0; j < arr.length ; j++){
             //행의 합
             first += arr[i][j];
-
-            //대각선의 합
-            third = arr[0][0];
-            third += arr[j+1][i+1];
-        }
-        max.push(first);
-        //max.push(third);
-        //sum1 += first + '/';
-        first = 0;
-        //third = 0;
-    }
-    //열의 합
-    for(let i = 0 ; i < arr.length ; i ++){
-        for (let j = 0 ; j < arr.length ; j ++){
+            //열의 합
             second += arr[j][i];
         }
-        //sum2 += second + '/';
-        max.push(second);
+        first = 0;
         second  = 0;
+    }
+    //대각선의 합
+    third = 0;
+    third1  = 0;
+    for(let i = 0 ; i < arr.length ; i ++){
+            third += arr[i][i];
+            third1 += arr[i][arr.length-i-1];
 
     }
-    return max;
+    let max1 = Math.max(first,second,third,third1);
+    return max1;
 }
-
 let arr=[[10, 13, 10, 12, 15],
     [12, 39, 30, 23, 11],
     [11, 25, 50, 53, 15],
---
+++ ScriptStudy/Day12(1).js
@@ -0,0 +1,18 @@
+function solution (arr){
+    let dx = [-1,0,1,1];
+    let dy = [0,1,0,-1];
+
+    for(let i = 0 ; i < arr.length ; i++){
+        for (let j = 0; j < arr.length; j++){
+
+
+        }
+    }
+
+}
+let arr=[[5, 3, 7, 2, 3],
+        [3, 7, 1, 6, 1],
+        [7, 2, 5, 3, 4],
+        [4, 3, 6, 4, 1],
+        [8, 7, 3, 5, 2]];
+console.log(solution(arr));
