91e03ed55c9b47bf74c11c591990bd60eeebba41
WonJeongLee <lwj010101517@gmail.com>
2021-11-01 23:05:00 +0900

    script 파일 추가 및 문제풀이

--- ScriptStudy/Day12(1).js
+++ ScriptStudy/Day12(1).js
@@ -1,14 +1,23 @@
 function solution (arr){
     let dx = [-1,0,1,1];
     let dy = [0,1,0,-1];
+    let c = 0;
 
     for(let i = 0 ; i < arr.length ; i++){
         for (let j = 0; j < arr.length; j++){
 
+            for(let k = 0; k <dx.length; k++){
+                for (let l = 0 ; l< dy.length; l++){
+                    if(arr[i][j] > arr[i+dx[k]][j+dy[l]]) {
+                        c++;
+                    }
+                }
+            }
+
 
         }
     }
-
+    return c;
 }
 let arr=[[5, 3, 7, 2, 3],
         [3, 7, 1, 6, 1],
