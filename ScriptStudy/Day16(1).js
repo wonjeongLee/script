0ee6b2f53d0dd0800ab235357b08b3bb23ea81ff
WonJeongLee <lwj010101517@gmail.com>
2021-11-16 21:16:33 +0900

    script  문제풀이

--- ScriptStudy/Day16(1).js
+++ ScriptStudy/Day16(1).js
@@ -1,20 +1,9 @@
+//자릿수의 합
 function solution(n, arr){
-    let answer, max=Number.MIN_SAFE_INTEGER;
-    for(let x of arr){
-        let sum=0, tmp=x;
-        while(tmp){
-            sum+=(tmp%10);
-            tmp=Math.floor(tmp/10);
-        }
-        if(sum>max){
-            max=sum;
-            answer=x;
-        }
-        else if(sum===max){
-            if(x>answer) answer=x;
-        }
-    }
-    return answer;
+
+    console.log(arr[0].split(''));
+
+    //return answer;
 }
 
 let arr=[128, 460, 603, 40, 521, 137, 123];
