e6c439d935b3195ee2da27d9cebe2ad00fca96de
Wonjeong lee <lwj010101517@gmail.com>
2021-11-21 01:57:46 +0900

    script  문제풀이

--- ScriptStudy/Day22(1).js
+++ ScriptStudy/Day22(1).js
@@ -1,20 +1,15 @@
 function solution(participant, completion) {
-    var answer = '';
+    var answer ='';
     let p1 =0, p2=0;
-    let p = participant.sort();
-    let c = completion.sort();
+    participant.sort();
+    completion.sort();
 
-    while (p1 < p.length && p2 < c.length) {
-        console.log(p1)
-        if (p[p1] !== c[p2]) {
-            answer.push(p[p1++]);
-        }else {
-            answer.push(c[p2++]);
+    for(let i = 0 ; i < participant.length ; i++){
+        if(participant[i] != completion[i]) {
+            answer = participant[i];
+            break;
         }
     }
-    while(p1<p.length) answer.push(p[p1++]);
-    while(p2<c.length) answer.push(c[p2++]);
-
     return answer;
 }
 
