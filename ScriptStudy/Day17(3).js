3e67d50d0dd6c2b4ac87a3b5033ecc9f5750adae
Wonjeong lee <lwj010101517@gmail.com>
2021-11-26 23:57:10 +0900

    script  문제풀이

--- ScriptStudy/Day17(3).js
+++ ScriptStudy/Day17(3).js
@@ -1,17 +1,26 @@
-function solution(n, k, card){
-    let answer;
-    let tmp = new Set();
-    for(let i=0; i<n; i++){
-        for(let j=i+1; j<n; j++){
-            for(let k=j+1; k<n; k++){
-                tmp.add(card[i]+card[j]+card[k]);
-            }
+//연속부분수열1
+function solution(m,arr){
+    let p1=p2=0;            let a=[1, 2, 1, 3, 1, 1, 1, 2];
+    console.log(solution(6, a));
+    let answer = cnt = 0;
+    let n = arr.length;
+    console.lof(n)
+
+    let b = 0;
+    while(p1 < n && p2 < n){
+        // p2 를 옮기면서 p1 에서 계속 더한다
+        // 만약 더한값이 6을 넘긴다면 p1 증가시킨다.
+        console.log(arr[p1]);
+        b += arr[p2];
+        p2++;
+        if(b > 6) {
+            cnt ++;
+            p1++;
         }
+
     }
-    let a=Array.from(tmp).sort((a, b)=>b-a);
-    answer=a[k-1];
     return answer;
 }
 
-let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
-console.log(solution(10, 3, arr));
+let a=[1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
