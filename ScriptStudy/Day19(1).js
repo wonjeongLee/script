ac55b3313c61f4a40aa14c7f50a2cdccb56fee7f
Wonjeong lee <lwj010101517@gmail.com>
2021-11-28 23:36:07 +0900

    script  문제풀이

--- ScriptStudy/Day19(1).js
+++ ScriptStudy/Day19(1).js
@@ -1,19 +1,13 @@
-function solution(arr1, arr2){
-    let answer=[];
-    arr1.sort();
-    arr2.sort();
-    let p1=p2=0;
-    while(p1<arr1.length && p2<arr2.length){
-        if(arr1[p1]==arr2[p2]){
-            answer.push(arr1[p1++]);
-            p2++;
-        }
-        else if(arr1[p1]<arr2[p2]) p1++;
-        else p2++;
+function solution(k, arr){
+    let answer, sum=0;
+    for(let i=0; i<k; i++) sum+=arr[i];
+    answer=sum;
+    for(let i=k; i<arr.length; i++){
+        sum+=(arr[i]-arr[i-k]);
+        answer=Math.max(answer, sum);
     }
     return answer;
 }
 
-let a=[1, 3, 9, 5, 2];
-let b=[3, 2, 5, 7, 8];
-console.log(solution(a, b));
+let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+console.log(solution(3, a));
