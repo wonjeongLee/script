b2ace9f6854db5be378d2c9ab731ed830df8f52d
WonJeongLee <lwj010101517@gmail.com>
2021-11-29 23:12:43 +0900

    script  문제풀이

--- ScriptStudy/Day17(2).js
+++ ScriptStudy/Day17(2).js
@@ -1,30 +1,30 @@
-//공통 원소 구하기
-function solution(arr1, arr2){
-    let answer = [];
-    n = arr1.sort().length;
-    m = arr2.sort().length;
-    let l = 0;
-    let P1 = P2 = 0;
 
-    if( n <= m )  l = n
-    else  l = m
-    console.log(l)
-
-   while(P1 < l && P2 < l){
-        if(arr1[P1] === arr2[P2]) {
-            answer.push(arr1[P1++])
-            P2 ++;
-        }else if (arr1[P1] < arr2[P2]){
-            P1 ++;
-        } else{
-            P2 ++;
-        }
-   }
-    return answer;
 }
 
 let a=[1, 3, 9];
 let b=[3, 2, 5, 7, 8];
 console.log(solution(a, b));
 // 1 2 3 5 9
-// 2 3 5 7 8
+// 2 3 5 7 8//공통 원소 구하기
+// function solution(arr1, arr2){
+//     let answer = [];
+//     n = arr1.sort().length;
+//     m = arr2.sort().length;
+//     let l = 0;
+//     let P1 = P2 = 0;
+//
+//     if( n <= m )  l = n
+//     else  l = m
+//     console.log(l)
+//
+//    while(P1 < l && P2 < l){
+//         if(arr1[P1] === arr2[P2]) {
+//             answer.push(arr1[P1++])
+//             P2 ++;
+//         }else if (arr1[P1] < arr2[P2]){
+//             P1 ++;
+//         } else{
+//             P2 ++;
+//         }
+//    }
+//     return answer;
--- ScriptStudy/Day20(1).js
+++ ScriptStudy/Day20(1).js
@@ -1,15 +1,20 @@
+//학급회장
 function solution(s){
     let answer;
-    let stack=[];
+    let sH = new Map();
     for(let x of s){
-        if(x===')'){
-            while(stack.pop()!=='(');
-        }
-        else stack.push(x);
+        if(sH.has(x)) sH.set(x, sH.get(x)+1);
+        else sH.set(x, 1);
     }
-    answer=stack.join('');
+    let max=Number.MIN_SAFE_INTEGER;
+    for(let [key, val] of sH){
+        if(val>max){
+            max=val;
+            answer=key;
+        }
+    }
     return answer;
 }
 
-let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+let str="BACBACCACCBDEDE";
 console.log(solution(str));
